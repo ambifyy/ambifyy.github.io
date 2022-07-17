3c53001f9108aa21e91129c8177d97067fcb5516
ambifyy <ambify3@gmail.com>
2022-07-16 23:38:57 -0400

    Sus

--- src/index.js
+++ src/index.js
@@ -19,12 +19,13 @@ import 'bootstrap/dist/js/bootstrap.js';
 import $ from 'jquery';
 import Popper from 'popper.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
